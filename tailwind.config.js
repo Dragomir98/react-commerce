f4b270c2cb2b1340fddb05c3ce61a4721eab5d0f
Dragomir98 <dragomir.stoychev98@gmail.com>
2021-10-09 19:56:01 +0300

    UI configurations

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,24 +8,30 @@ module.exports = {
       lg: "976px",
       xl: "1440px",
     },
-    colors: {
-      primary: {
-        default: "#2f2a38",
-      },
-      secondary: {
-        default: "#8742f5",
-      },
-      alt: {
-        default: "#d6d3db",
-      },
-      success: "#218041",
-      error: "#912020",
-      hover: {
-        card: "",
-        link: "#a392be",
+    maxWidth: {
+      "1/4": "25%",
+      "1/2": "50%",
+      "3/4": "75%",
+    },
+    extend: {
+      colors: {
+        primary: {
+          default: "#2f2a38",
+        },
+        secondary: {
+          default: "#8742f5",
+        },
+        alt: {
+          default: "#d6d3db",
+        },
+        success: "#218041",
+        error: "#912020",
+        hover: {
+          card: "",
+          link: "#a392be",
+        },
       },
     },
-    extend: {},
   },
   variants: {
     extend: {},
